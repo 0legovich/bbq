ae595da82d927c82bd1a8087d47f03b6fa7fdb22
0legovich <romq1@mail.ru>
2017-09-14 17:19:22 +0300

    add check_email for Subscription

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -6,8 +6,17 @@ class Subscription < ActiveRecord::Base
   validates :user_name, presence: true, unless: 'user.present?'
   validates :user_email, presence: true, format: /\A[a-zA-Z0-9_.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-.]+\z/, unless: 'user.present?'
 
-  validates :user, uniqueness: { scope: :event_id }, if: 'user.present?'
-  validates :user_email, uniqueness: { scope: :event_id }, unless: 'user.present?'
+  validates :user, uniqueness: {scope: :event_id}, if: 'user.present?'
+  validates :user_email, uniqueness: {scope: :event_id}, unless: 'user.present?'
+
+  before_create :check_email
+
+  def check_email
+    if !user.present? && !User.find_by_email(self.user_email).nil?
+      errors.add(:user_email, :invalid, message: 'уже используется другим пользователем')
+      false
+    end
+  end
 
   def user_name
     if user.present?
