dfe0f0926ea8d3199bdcab439c3136b4fdece645
0legovich <romq1@mail.ru>
2017-09-18 23:24:19 +0300

    correction carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,11 @@
 if Rails.env.production?
-  Carrierwaveconfigure do |config|
+  CarrierWave.configure do |config|
     config.fog_credentails = {
       provider: 'AWS',
       aws_access_key_id: ENV['S3_ACCESS_KEY'],
       aws_secret_access_key: ENV['S3_SECRET_KEY']
     }
 
-    config.flog_directory = ENV['S3_BUCKET_NAME']
+    config.fog_directory = ENV['S3_BUCKET_NAME']
   end
 end
